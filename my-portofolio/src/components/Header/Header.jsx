dc901fb57e1c197f94655e324a0650c007943786
asrafmi <asraf.muhammad07@gmail.com>
2024-03-23 00:01:30 +0700

    feat: finish implement drawer

--- my-portofolio/src/components/Header/Header.jsx
+++ my-portofolio/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import {
   Bars3Icon,
   MoonIcon,
   SunIcon,
-  ChevronRightIcon,
+  XMarkIcon,
 } from '@heroicons/react/24/outline';
 import Drawer from 'react-modern-drawer';
 import 'react-modern-drawer/dist/index.css';
@@ -66,12 +66,48 @@ const Header = () => {
         open={isOpen}
         onClose={toggleDrawer}
         direction="right"
-        className="bla bla bla"
+        className={styles.drawer}
       >
         <div className={styles.drawerContainer}>
-          <img src="/logo.png" alt="logo" />
+          <div className={styles.drawerHeaderContainer}>
+            <img src="/logo.png" alt="logo" />
+            <XMarkIcon onClick={toggleDrawer} className={styles.closeIcon} />
+          </div>
           <hr />
-          <h1 style={{ color: 'red' }}>Hello World</h1>
+          <div className={styles.listMenuContainer}>
+            <ul className={styles.listMenu}>
+              <li>
+                <a href="/">About</a>
+              </li>
+              <li>
+                <a href="/">Work</a>
+              </li>
+              <li>
+                <a href="/">Testimonials</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <hr />
+          <div className={styles.swithThemeContainer}>
+            <p>Switch Theme</p>
+            {isLight ? (
+              <SunIcon
+                onClick={() => setIsLight(!isLight)}
+                className={styles.sunIcon}
+              />
+            ) : (
+              <MoonIcon
+                onClick={() => setIsLight(!isLight)}
+                className={styles.moonIcon}
+              />
+            )}
+          </div>
+          <button className={styles.downloadBtn}>
+            <p>Download CV</p>
+          </button>
         </div>
       </Drawer>
     </>
